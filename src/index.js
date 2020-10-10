63e47db358650d4fa19ce98e8514b8bd50399ba4
Zachary Melman <zachary.s.melman@gmail.com>
2020-10-10 12:27:16 -0700

    added logout button to footer

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from "react-router-dom";
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import './index.css';
 
 
 ReactDOM.render(
   <React.Fragment>
 
     <BrowserRouter> 
-
       <App />
       
     </BrowserRouter>
